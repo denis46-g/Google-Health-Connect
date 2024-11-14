0a58a4b7ed5024cb2d6c5ee12600868a988ef867
Acer <goskovd2017@mail.ru>
2024-11-14 20:39:43 +0300

    Initial commit

---
+++ app/src/main/java/com/example/googlehealthconnect/Permissions.kt
@@ -0,0 +1,17 @@
+package com.example.googlehealthconnect
+
+import androidx.health.connect.client.permission.HealthPermission
+import androidx.health.connect.client.records.HeartRateRecord
+import androidx.health.connect.client.records.StepsRecord
+import androidx.health.connect.client.records.WeightRecord
+
+// Create a set of permissions for required data types
+val PERMISSIONS =
+    setOf(
+        HealthPermission.getReadPermission(StepsRecord::class),
+        HealthPermission.getWritePermission(StepsRecord::class),
+        HealthPermission.getReadPermission(HeartRateRecord::class),
+        HealthPermission.getWritePermission(HeartRateRecord::class),
+        HealthPermission.getReadPermission(WeightRecord::class),
+        HealthPermission.getWritePermission(WeightRecord::class)
+    )
